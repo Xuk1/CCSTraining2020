7fcad0c15945200d82c6dd41b73d053dcafba60a
bluesadi <liuyibo@stu.scu.edu.cn>
2020-07-17 23:14:26 +0800

    add email

--- src/main/java/com/ilovemengmei/ccstraining2020/domain/User.java
+++ src/main/java/com/ilovemengmei/ccstraining2020/domain/User.java
@@ -5,6 +5,7 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private String email;
 
     public String getPassword() {
         return password;
@@ -30,12 +31,21 @@ public class User {
         this.id = id;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
